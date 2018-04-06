1dd2e38e8f6d4a14e382cf7ec9ac5780e46b0d58
chathika <chathikagunaratne@gmail.com>
2018-04-06 18:51:50 -0400

    fixed an error

--- BuildServer.bat
+++ BuildServer.bat
@@ -1,6 +1,6 @@
-javac -d bin -cp "C:/Users/ch328575/AppData/Local/Continuum/Anaconda2/share/py4j/py4j0.10.6.jar;C:/Program Files/NetLogo 6.0.2/app/netlogo-6.0.2.jar;." src/bsearch/space/*.java src/bsearch/nlogolink/*.java src/emd/server/*.java
-cp src/Manifest.txt bin
+javac -d bin -cp "C:/Users/ch328575/AppData/Local/Continuum/Anaconda2/share/py4j/py4j0.10.6.jar;C:/Program Files/NetLogo 6.0.2/app/netlogo-6.0.2.jar;." src/server/bsearch/space/*.java src/server/bsearch/nlogolink/*.java src/server/*.java
+cp src/server/Manifest.txt bin
 cd bin
-jar cvfm NetLogoControllerServer.jar Manifest.txt emd/server/NetLogoControllerServer.class bsearch emd
+jar cvfm NetLogoControllerServer.jar Manifest.txt ./emd/server/NetLogoControllerServer.class bsearch 
 cd ..
 
