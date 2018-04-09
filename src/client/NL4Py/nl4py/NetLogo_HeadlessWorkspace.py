296c845bf6654d92286e9f8f88be9caff78f58a3
chathika <chathikagunaratne@gmail.com>
2018-04-09 19:09:34 -0400

    fixed getParamSpace and setParamsRandom functions

--- src/client/NL4Py/nl4py/NetLogo_HeadlessWorkspace.py
+++ src/client/NL4Py/nl4py/NetLogo_HeadlessWorkspace.py
@@ -14,6 +14,7 @@ GNU General Public License for more details.
 You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.'''
 
+import py4j
 from py4j.java_gateway import JavaGateway
 from py4j.protocol import Py4JNetworkError
 import subprocess
@@ -28,6 +29,8 @@ class NetLogo_HeadlessWorkspace:
     __bridge = None
     __gateway = None 
     __session = None
+    __path = None
+
     def __init__(self):
         self.__gateway = JavaGateway()# New gateway connection
         self.__bridge = self.__gateway.entry_point
@@ -40,7 +43,8 @@ class NetLogo_HeadlessWorkspace:
     '''Returns a session id for this controller to be used for further use of this ABM'''
     def openModel(self, path):
         #try:
-            self.__session = self.__bridge.openModel(path)
+        self.__path = path
+        self.__session = self.__bridge.openModel(path)
         #except Py4JNetworkError, e:
             #raise NL4PyControllerServerException("Did you copy the NetLogoControllerServer.jar into your NetLogo/app folder? Or maybe the socket is busy? Trying running NL4Py.NLCSStarter.shutdownServer()"), None, sys.exc_info()[2]
     '''Sends a signal to the server to tell the respective controller to close its'''
@@ -66,25 +70,25 @@ class NetLogo_HeadlessWorkspace:
         return result
     '''Sends a signal to the server to tell the respective controller to get the'''
     '''parameter specs of its HeadlessWorkspace object'''
-    def getParamSpace(self, path):
-        self.__bridge.getParamList(self.__session,path)
+    def getParamSpace(self):
+        return self.__bridge.getParamList(self.__session,self.__path)
     #An extra helpful method:
     '''Sets the parameters randomly through the JavaGateway using'''
     '''Random parameter initialization code from BehaviorSearch'''
-    def setParamsRandom(self, model_string):
-        paramSpecs = self.__bridge.getParamList(self.__session, model_string).getParamSpecs()
+    def setParamsRandom(self):
+        paramSpecs = self.__bridge.getParamList(self.__session, self.__path).getParamSpecs()
         
         ##Using some bsearch code here thanks to Forrest Stonedahl and the NetLogo team
         for paramSpec in paramSpecs:
-            if jg.is_instance_of(self.__gateway,paramSpec,"bsearch.space.DoubleDiscreteSpec"):
+            if py4j.java_gateway.is_instance_of(self.__gateway,paramSpec,"bsearch.space.DoubleDiscreteSpec"):
                 paramValue = paramSpec.generateRandomValue(self.__gateway.jvm.org.nlogo.api.MersenneTwisterFast())
-            if jg.is_instance_of(self.__gateway,paramSpec,"bsearch.space.DoubleContinuousSpec"):
+            if py4j.java_gateway.is_instance_of(self.__gateway,paramSpec,"bsearch.space.DoubleContinuousSpec"):
                 paramValue = paramSpec.generateRandomValue(self.__gateway.jvm.org.nlogo.api.MersenneTwisterFast())
-            if jg.is_instance_of(self.__gateway,paramSpec,"bsearch.space.CategoricalSpec"):
+            if py4j.java_gateway.is_instance_of(self.__gateway,paramSpec,"bsearch.space.CategoricalSpec"):
                 paramValue = paramSpec.generateRandomValue(self.__gateway.jvm.org.nlogo.api.MersenneTwisterFast())
                 if type(paramValue) != bool:#isinstance(data[i][k], bool)
                     paramValue = '"{}"'.format(paramSpec.generateRandomValue(self.__gateway.jvm.org.nlogo.api.MersenneTwisterFast()))
-            if jg.is_instance_of(self.__gateway,paramSpec,"bsearch.space.ConstantSpec"):
+            if py4j.java_gateway.is_instance_of(self.__gateway,paramSpec,"bsearch.space.ConstantSpec"):
                 paramValue = paramSpec.generateRandomValue(self.__gateway.jvm.org.nlogo.api.MersenneTwisterFast())
             print("NetLogo command: set " + str(paramSpec.getParameterName()) + " " + str(paramValue))
             self.__bridge.command(self.__session, "set " + str(paramSpec.getParameterName()) + " " + str(paramValue))
