ba2c64f594f3d0bb286a87140e1e0a7791203ce9
chathika <chathikagunaratne@gmail.com>
2018-04-11 03:39:10 -0400

    Build instructions change

--- BuildServer.bat
+++ BuildServer.bat
@@ -1,6 +1,6 @@
 javac -d bin -cp "C:/Users/ch328575/AppData/Local/Continuum/Anaconda2/share/py4j/py4j0.10.6.jar;C:/Program Files/NetLogo 6.0.2/app/netlogo-6.0.2.jar;." src/server/bsearch/space/*.java src/server/bsearch/nlogolink/*.java src/server/*.java
 cp src/server/Manifest.txt bin
 cd bin
-jar cvfm NetLogoControllerServer.jar Manifest.txt bsearch emd
+jar cvfm NetLogoControllerServer.jar Manifest.txt bsearch nl4py
 cd ..
 
