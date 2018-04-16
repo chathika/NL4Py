05986238c69131ac435c63fc2f286e3c242d6409
chathika <chathikagunaratne@gmail.com>
2018-04-15 22:17:43 -0400

    client updates

---
+++ src/client/NL4Py/build/lib/nl4py/NetLogo_HeadlessWorkspace.py
@@ -0,0 +1,90 @@
+'''NL4Py, A NetLogo controller for Python
+Copyright (C) 2018  Chathika Gunaratne
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.'''
+
+from py4j.java_gateway import JavaGateway
+from py4j.protocol import Py4JNetworkError
+import subprocess
+import threading
+import os
+import atexit
+import sys
+
+
+'''Responsible for communicating with the NetLogo controller, a Java executable'''
+class NetLogo_HeadlessWorkspace:
+    __bridge = None
+    __gateway = None 
+    __session = None
+    def __init__(self):
+        self.__gateway = JavaGateway()# New gateway connection
+        self.__bridge = self.__gateway.entry_point
+    
+    #######################################################
+    ###Below are public functions of the NL4Py interface###
+    #######################################################
+    
+    '''Opens a NetLogo model and creates a headless workspace controller on the server'''
+    '''Returns a session id for this controller to be used for further use of this ABM'''
+    def openModel(self, path):
+        #try:
+            self.__session = self.__bridge.openModel(path)
+        #except Py4JNetworkError, e:
+            #raise NL4PyControllerServerException("Did you copy the NetLogoControllerServer.jar into your NetLogo/app folder? Or maybe the socket is busy? Trying running NL4Py.NLCSStarter.shutdownServer()"), None, sys.exc_info()[2]
+    '''Sends a signal to the server to tell the respective controller to close its'''
+    '''HeadlessWorkspace object'''
+    def closeModel(self):
+        self.__bridge.closeModel(self.__session)
+    '''Sends a signal to the server to tell the respective controller to create a'''
+    '''new instance of its HeadlessWorkspace object'''
+    def refresh(self):
+        self.__bridge.refresh(self.__session)
+    '''Sends a signal to the server to tell the respective controller to export the view'''
+    ''' of its HeadlessWorkspace object'''
+    def exportView(self, filename):
+        self.exportView(self.__session, filename)
+    '''Sends a signal to the server to tell the respective controller to send a'''
+    '''NetLogo command to its HeadlessWorkspace object'''
+    def command(self, command):
+        self.__bridge.command(self.__session,command)
+    '''Sends a signal to the server to tell the respective controller to execute a'''
+    '''reporter on its HeadlessWorkspace object'''
+    def report(self, command):
+        result = self.__bridge.report(self.__session, command)
+        return result
+    '''Sends a signal to the server to tell the respective controller to get the'''
+    '''parameter specs of its HeadlessWorkspace object'''
+    def getParamSpace(self, path):
+        self.__bridge.getParamList(self.__session,path)
+    #An extra helpful method:
+    '''Sets the parameters randomly through the JavaGateway using'''
+    '''Random parameter initialization code from BehaviorSearch'''
+    def setParamsRandom(self, model_string):
+        paramSpecs = self.__bridge.getParamList(self.__session, model_string).getParamSpecs()
+        
+        ##Using some bsearch code here thanks to Forrest Stonedahl and the NetLogo team
+        for paramSpec in paramSpecs:
+            if jg.is_instance_of(self.__gateway,paramSpec,"bsearch.space.DoubleDiscreteSpec"):
+                paramValue = paramSpec.generateRandomValue(self.__gateway.jvm.org.nlogo.api.MersenneTwisterFast())
+            if jg.is_instance_of(self.__gateway,paramSpec,"bsearch.space.DoubleContinuousSpec"):
+                paramValue = paramSpec.generateRandomValue(self.__gateway.jvm.org.nlogo.api.MersenneTwisterFast())
+            if jg.is_instance_of(self.__gateway,paramSpec,"bsearch.space.CategoricalSpec"):
+                paramValue = paramSpec.generateRandomValue(self.__gateway.jvm.org.nlogo.api.MersenneTwisterFast())
+                if type(paramValue) != bool:#isinstance(data[i][k], bool)
+                    paramValue = '"{}"'.format(paramSpec.generateRandomValue(self.__gateway.jvm.org.nlogo.api.MersenneTwisterFast()))
+            if jg.is_instance_of(self.__gateway,paramSpec,"bsearch.space.ConstantSpec"):
+                paramValue = paramSpec.generateRandomValue(self.__gateway.jvm.org.nlogo.api.MersenneTwisterFast())
+            print("NetLogo command: set " + str(paramSpec.getParameterName()) + " " + str(paramValue))
+            self.__bridge.command(self.__session, "set " + str(paramSpec.getParameterName()) + " " + str(paramValue))
---
+++ src/client/NL4Py/build/lib/nl4py/__init__.py
@@ -0,0 +1,40 @@
+'''NL4Py, A NetLogo controller for Python
+Copyright (C) 2018  Chathika Gunaratne
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.'''
+
+from nl4py.NetLogo_HeadlessWorkspace import NetLogo_HeadlessWorkspace
+from nl4py.NetLogo_Controller_Server_Starter import NetLogo_Controller_Server_Starter
+
+import urllib.request
+import shutil
+import os
+if not os.path.exists("./server/"):
+    os.makedirs("./server/")
+
+print("Downloading the NetLogo Controller Server jar file...")
+url = 'https://github.com/chathika/NL4Py/raw/master/bin/NetLogoControllerServer.jar'
+# Download the file from `url` and save it locally under `file_name`:
+with urllib.request.urlopen(url) as response, open('./server/NetLogoControllerServer.jar', 'wb') as out_file:
+    shutil.copyfileobj(response, out_file)
+print("Downloading the Py4j jar file...")
+url = 'https://github.com/chathika/NL4Py/raw/master/lib/py4j0.10.6.jar'
+# Download the file from `url` and save it locally under `file_name`:
+with urllib.request.urlopen(url) as response, open('./server/py4j0.10.6.jar', 'wb') as out_file:
+    shutil.copyfileobj(response, out_file)
+print("Dependencies installed successfully!")
+print("Starting up server...")
+NLCSStarter = NetLogo_Controller_Server_Starter()
+NLCSStarter.startServer()
+print("Server up and ready!")
