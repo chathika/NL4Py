02f9afcb4e634f8d7a09b7c214b27f5abc67cec0
chathika <chathikagunaratne@gmail.com>
2018-04-27 14:40:22 +0000

    Update to experiment 2

--- JASSS2018/JASSS2018_TEST1.2.py
+++ JASSS2018/JASSS2018_TEST1.2.py
@@ -11,62 +11,78 @@ import nl4py
 import sys
 import time
 import math
-
-def doNRuns(runsNeeded):
+def openModelAndRun(workspace_, model_):
+	workspace_.openModel(model_)
+	workspace_.setParamsRandom()
+	reporters = ["ticks", "not any? turtles", "not any? wolves and count sheep > max-sheep"]
+	workspace_.command("setup")
+	workspace_.scheduleReportersAndRun(reporters, 0,1,100, "go")
+def doNRuns(runsNeeded, threadCount_):
 	model = "./Wolf Sheep Predation.nlogo"
 	startTime = int(round(time.time() * 1000))
 	runsDone = 0
 	sheepCount = []
 	wolfCount = []
 	workspacesRunning = []
-	threadCount = 8
-	while runsDone != runsNeeded:
-		runningWorkspacesCount = int(runsNeeded - runsDone if (runsNeeded - runsDone) <= threadCount else threadCount)
-		for i in range(0,int(runningWorkspacesCount)):
-			n = nl4py.netlogoWorkspaceFactory.newNetLogoHeadlessWorkspace()
-		workspacesRunning = nl4py.netlogoWorkspaceFactory.getAllExistingWorkspaces()
+	threadCount = threadCount_
+
+	runningWorkspacesCount = int(runsNeeded - runsDone if (runsNeeded - runsDone) <= threadCount else threadCount)
+	for i in range(0,int(runningWorkspacesCount)):
+		n = nl4py.netlogoWorkspaceFactory.newNetLogoHeadlessWorkspace()
+	for workspace in nl4py.netlogoWorkspaceFactory.getAllExistingWorkspaces():
+		openModelAndRun(workspace, model)
+	runsStarted = len(nl4py.netlogoWorkspaceFactory.getAllExistingWorkspaces())
+#	time.sleep(0.05)
+	while(runsDone < runsNeeded):
+#		print(runsDone)
 		for workspace in nl4py.netlogoWorkspaceFactory.getAllExistingWorkspaces():
-			workspace.openModel(model)
-			workspace.setParamsRandom()
-			reporters = ["ticks", "not any? turtles", "not any? wolves and count sheep > max-sheep"]
-			workspace.command("setup")
-			workspace.scheduleReportersAndRun(reporters, 0,1,100, "go")
-		while len(workspacesRunning) != 0:
-			time.sleep(0.05)
-			for workspace in workspacesRunning:
-				try:
-					workspaceResults = workspace.getScheduledReporterResults()
-				except:
-					workspacesRunning.remove(workspace)
-					workspace.deleteWorkspace()
+			try:
+				workspaceResults = workspace.getScheduledReporterResults()
 				if len(workspaceResults) > 0:
-					if workspaceResults[0][0] == 1000 or workspaceResults[0][1] or workspaceResults[0][2]:
+					if workspaceResults[0][0] == 100 or workspaceResults[0][1] or workspaceResults[0][2]:
 						sheepCount.append(workspace.report("count sheep"))
 						wolfCount.append(workspace.report("count wolves"))
-						workspacesRunning.remove(workspace)
+						nl4py.netlogoWorkspaceFactory.getAllExistingWorkspaces().remove(workspace)
 						workspace.deleteWorkspace()
-		runsDone = runsDone + runningWorkspacesCount
+						workspace = None
+						runsDone = runsDone + 1
+						if(runsDone == runsNeeded):
+							break
+						if(runsStarted != runsNeeded):
+							runsStarted = runsStarted + 1
+							replacement = nl4py.netlogoWorkspaceFactory.newNetLogoHeadlessWorkspace()
+							openModelAndRun(replacement,model)
+			except:
+				print("exception")
+				nl4py.netlogoWorkspaceFactory.getAllExistingWorkspaces().remove(workspace)
+				workspace.deleteWorkspace()
+				if(runsStarted != runsNeeded):
+					runsStarted = runsStarted + 1
+					replacement = nl4py.netlogoWorkspaceFactory.newNetLogoHeadlessWorkspace()
+					openModelAndRun(replacement,model)
 	stopTime = int(round(time.time() * 1000))
 	totalTime = stopTime - startTime
 #	print(sheepCount)
 #	print(wolfCount)
-	sheepCount = []
+	print(runsDone)
 	wolfCount = []
+	sheepCount = []
 	return totalTime
 print("\n1 Starting the NetLogoControllerServer with: nl4py.startServer()\n")
 nl4py.startServer()
 time.sleep(2)
 allTimes = []
-for j in range(0,30):
-	timeIteration = []
-	for i in range(1,15):
-		timeTaken = doNRuns(math.pow(2,i))
-		timeIteration.append(timeTaken)
-		print(timeTaken)
-		nl4py.netlogoWorkspaceFactory.deleteAllExistingWorkspaces() 
-#		time.sleep(5)
-	allTimes.append(timeIteration)
-	print(allTimes)
+for j in range(0,5):
+	for i in range(10,16):
+		for threadCount in [1,2,4,8,16]:
+			print("Runs ", i*1000, "Threads " , threadCount)
+			timeTaken = doNRuns(i*1000, threadCount)
+			allTimes.append([i,threadCount,timeTaken])
+			print(allTimes)
+			try:
+				nl4py.netlogoWorkspaceFactory.deleteAllExistingWorkspaces()
+			except:
+				pass
 print(allTimes)
 import pandas as pd
 p = pd.DataFrame(allTimes)
