c99d070f56885a13540663015f91a3b66abb4747
chathika <chathikagunaratne@gmail.com>
2018-05-04 01:10:56 -0400

    server: exception handling

--- src/server/HeadlessWorkspaceController.java
+++ src/server/HeadlessWorkspaceController.java
@@ -103,7 +103,7 @@ public class HeadlessWorkspaceController extends NetLogoController {
 									try{
 										for(String reporter : reporters) {
 											//record results
-											reporterResult = ws.report(reporter).toString();
+											reporterResult = ws.report("carefully [show " + reporter+"][show 0]").toString();
 											reporterResults.add(reporterResult);
 										}
 									} catch (org.nlogo.nvm.RuntimePrimitiveException e) {
@@ -114,7 +114,6 @@ public class HeadlessWorkspaceController extends NetLogoController {
 										System.out.println("Exception when collecting scheduled reporter results");
 										System.out.println(reporterResult);
 										System.out.println(reporterResults);
-										System.exit(10);
 										//This can throw a netlogo exception if the model is done running due to custom stop condition
 										continue;
 									} catch (Exception e) {
@@ -122,7 +121,6 @@ public class HeadlessWorkspaceController extends NetLogoController {
 										for(String reporter : reporters) {
 											reporterResults.add("Bad NetLogo syntax caused exception");					
 										}
-										System.exit(10);
 									} finally {
 										for(String resultI : reporterResults) {
 											scheduledReporterResults.put(resultI);
