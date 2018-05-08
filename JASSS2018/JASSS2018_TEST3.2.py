86f93fd7b75db73c235c8f51953ab861eaaf8a3c
chathika <chathikagunaratne@gmail.com>
2018-05-08 18:28:45 +0000

    New experiment

---
+++ JASSS2018/JASSS2018_TEST3.2.py
@@ -0,0 +1,34 @@
+import time
+startTime = int(round(time.time() * 1000))
+import pyNetLogo
+workspaces = []
+for i in range(0,200):
+	n = pyNetLogo.NetLogoLink(gui=False, netlogo_home = "/home/ubuntu/netlogo", netlogo_version = '6')
+	n.load_model("./Ethnocentrism.nlogo")
+        n.command("set mutation-rate random-float 1")
+        n.command("set initial-PTR random-float 1")
+        n.command("set death-rate random-float 1")
+        n.command("set cost-of-giving random-float 1")
+        n.command("set immigrants-per-day random 100")
+        n.command("set gain-of-receiving random-float 1")
+        n.command("set immigrant-chance-cooperate-with-same random-float 1")
+        n.command("set immigrant-chance-cooperate-with-different random-float 1")
+        n.command("setup-full")
+        n.command("repeat 1000 [go]")
+	workspaces.append(n)
+while len(workspaces) > 0:
+	for workspace in workspaces:
+		#Check if workspaces are stopped
+		ticks = int(float(workspace.report("ticks")))
+		if ticks == 1000:
+			r1 = workspace.report('count turtles with [shape = "circle"]')
+                        r2 = workspace.report('count turtles with [shape = "circle 2"]')
+                        r3 = workspace.report('count turtles with [shape = "square"]')
+                        r4 = workspace.report('count turtles with [shape = "square 2"]')
+                        print(str(200 - len(workspaces))+" "+str(r1)+" "+str(r2)+" "+str(r3)+" "+str(r4)+" "+str(ticks))
+			workspaces.remove(workspace)
+stopTime = int(round(time.time() * 1000))
+totalTime = stopTime - startTime
+with open("Times_Comparison_Ethnocentrism_2.csv", "a+") as myfile:
+        myfile.write('Ethnocentrism,200,PyNetLogo,' + str(totalTime))
+print(totalTime)
