a052ad507a731d6f771ade05eaedfed6ac07e3d3
chathika <chathikagunaratne@gmail.com>
2018-05-09 13:44:01 -0400

    server: thread safety fix

--- src/server/HeadlessWorkspaceController.java
+++ src/server/HeadlessWorkspaceController.java
@@ -74,52 +74,51 @@ public class HeadlessWorkspaceController extends NetLogoController {
 							//Now execute the schedule
 							//Has start time passed?
 							int ticksAtStart = ((Double)ws.report("ticks")).intValue();
-							synchronized(mon) {
-								if(ticksAtStart <= startAtTick ){
-									int tickCounter = ticksAtStart;
-									double ticksOnModel = ticksAtStart;
-									boolean modelStopped = false;
-									while (!modelStopped && controllerNeeded && (tickCounter < stopAtTick || stopAtTick < 0)) {
-										//tick the interval
-										/*for (int i = 0; i < intervalTicks; i ++ ){
-											//go
-											ws.command(goCommand);
-											//increment counter
-											tickCounter++;
-										}*/
-										
-										ws.command("repeat " + Integer.toString(intervalTicks) +" [" + goCommand + "]");
-										tickCounter += tickCounter + intervalTicks;
-										if (tickCounter > stopAtTick){
-												modelStopped = true;
-												break;
-										}
-										double ticksOnModelNew = (Double)ws.report("ticks");
-										if(ticksOnModel == ticksOnModelNew){
-											//Model has stopped, no tick progression
+							if(ticksAtStart <= startAtTick ){
+								int tickCounter = ticksAtStart;
+								double ticksOnModel = ticksAtStart;
+								boolean modelStopped = false;
+								while (!modelStopped && controllerNeeded && (tickCounter < stopAtTick || stopAtTick < 0)) {
+									//tick the interval
+									/*for (int i = 0; i < intervalTicks; i ++ ){
+										//go
+										ws.command(goCommand);
+										//increment counter
+										tickCounter++;
+									}*/
+									
+									ws.command("repeat " + Integer.toString(intervalTicks) +" [" + goCommand + "]");
+									tickCounter += tickCounter + intervalTicks;
+									if (tickCounter > stopAtTick){
 											modelStopped = true;
 											break;
-										} else {
-											ticksOnModel = ticksOnModelNew;
-										}
-										if(modelStopped){
-											break;
-										}
-										//run reporters
-										ArrayList<String> reporterResults = new ArrayList<String>();
-										for(String reporter : reporters) {
-											//record results
-											String reporterResult = report(reporter).toString();
-											reporterResults.add(reporterResult);
-										}
-										synchronized(scheduledReporterResults){
-											for(String resultI : reporterResults) {
-												scheduledReporterResults.put(resultI);
-											}
+									}
+									double ticksOnModelNew = (Double)ws.report("ticks");
+									if(ticksOnModel == ticksOnModelNew){
+										//Model has stopped, no tick progression
+										modelStopped = true;
+										break;
+									} else {
+										ticksOnModel = ticksOnModelNew;
+									}
+									if(modelStopped){
+										break;
+									}
+									//run reporters
+									ArrayList<String> reporterResults = new ArrayList<String>();
+									for(String reporter : reporters) {
+										//record results
+										String reporterResult = report(reporter).toString();
+										reporterResults.add(reporterResult);
+									}
+									synchronized(scheduledReporterResults){
+										for(String resultI : reporterResults) {
+											scheduledReporterResults.put(resultI);
 										}
 									}
 								}
-								scheduleDone = true;
+							}
+							synchronized(mon) {
 								mon.notifyAll();
 							}
 						} else {
@@ -258,22 +257,18 @@ public class HeadlessWorkspaceController extends NetLogoController {
 	}
 	public ArrayList<String> getScheduledReporterResults () {
 		ArrayList<String> results  = new ArrayList<String>();
-		//if (scheduleDone){
+		synchronized(this.mon){
 			try{
-				//synchronized(mon){
-				//	mon.wait();
-				//}
-				while(!scheduleDone) {Thread.sleep(10);}
+				this.mon.wait();
 			} catch (InterruptedException e) {
 				e.printStackTrace();
 			}
-			
 			try {	
 				scheduledReporterResults.drainTo(results);
 			} catch (Exception e) {
 				e.printStackTrace();
 			}
-		//}
+		}
 		return results;
 	}	
 	
