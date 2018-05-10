21c33bcfd80742e48b86631c7e48238eefa79775
Chathika Gunaratne <ch328575@ucf.edu>
2018-05-10 14:07:12 -0400

    Update Experiments

--- JASSS2018/JASSS2018_TEST1.2.py
+++ JASSS2018/JASSS2018_TEST1.2.py
@@ -1,82 +1,42 @@
-########Demonstration of NL4Py#################
-#This example takes two commandline arguments n and model
-#n : number of concurrent model runs required.
-#model: path to model file
-#example usage: python NRunsOfAModel.py 100 "/path/to/model.nlogo"
-###############################################
-
-print("\n\n------------ This is a Demonstration of NL4PY --------------------\n")
-
 import nl4py
-import sys
-import time
-import math
-def runModel(workspace_):
-	workspace_.command('stop')
-	workspace_.command('set initial-number-sheep random 250')
-	workspace_.command('set initial-number-wolves random 250')
-	workspace_.command('set grass-regrowth-time random 100')
-	workspace_.command('set sheep-gain-from-food random 50')
-	workspace_.command('set wolf-gain-from-food random 100')
-	workspace_.command('set sheep-reproduce (random 20) + 1')
-	workspace_.command('set wolf-reproduce random 20')
-	workspace_.command('set model-version "sheep-wolves-grass"') # make sure we run the right version of the model 
-	reporters = ["ticks", "not any? turtles", "not any? wolves and count sheep > max-sheep", "count sheep", "count wolves"]
-	workspace_.command("setup")
-	workspace_.scheduleReportersAndRun(reporters, 0,1,100, "go")
-def doNRuns(runsNeeded, threadCount_):
-	model = "./Wolf Sheep Predation.nlogo"
-	startTime = int(round(time.time() * 1000))
-	runsDone = 0
-	wolfsheepcounts = []
-	workspacesRunning = []
-	threadCount = threadCount_
-	runningWorkspacesCount = int(runsNeeded - runsDone if (runsNeeded - runsDone) <= threadCount else threadCount)
-	for i in range(0,int(runningWorkspacesCount)):
-		n = nl4py.newNetLogoHeadlessWorkspace()
-	for workspace in nl4py.getAllHeadlessWorkspaces():
-		workspace.openModel(model)
-		runModel(workspace)
-	runsStarted = len(nl4py.getAllHeadlessWorkspaces())
-	while(runsDone != runsNeeded):
-		for workspace in nl4py.getAllHeadlessWorkspaces():
-			workspaceResults = workspace.getScheduledReporterResults()
-			if len(workspaceResults) > 0:
-				ticks = int(float(workspaceResults[-1][0]))
-				stop1 = str(workspaceResults[-1][1]).lower() == "true"
-				stop2 = str(workspaceResults[-1][2]).lower() == "true"
-				if (ticks == 100) or stop1 or stop2:
-					wolfsheepcounts.append(workspaceResults)
-					runsDone = runsDone + 1
-					if(runsDone == runsNeeded):
-						break
-					if(runsStarted != runsNeeded):
-						runsStarted = runsStarted + 1
-						runModel(workspace)
-	stopTime = int(round(time.time() * 1000))
-	totalTime = stopTime - startTime
-	print(totalTime)
-	return totalTime
-print("\n1 Starting the NetLogoControllerServer with: nl4py.startServer()\n")
 nl4py.startServer()
-time.sleep(2)
-allTimes = []
-import pandas as pd
-for j in range(0,11):
-	for i in [100]:
-		for threadCount in [8]:
-			print("Runs ", i, "Threads " , threadCount)
-			timeTaken = doNRuns(i, threadCount)
-			allTimes.append([i,threadCount,timeTaken])
-			print(allTimes)
-			p = pd.DataFrame(allTimes)
-			p.to_csv("AllTimes_WolfSheepPredation.csv")
-			try:
-				nl4py.netlogoWorkspaceFactory.deleteAllExistingWorkspaces()
-			except:
-				pass
-print(allTimes)
-print("DONE____________________________________________________ALL DONE____________________________________________________")
-print(allTimes)
-print('\n3) Shutdown the server to release compute resources using: nl4py.stopServer()')
-nl4py.stopServer()
+
+import time
+startTime = int(round(time.time() * 1000))
+nl4py.deleteAllHeadlessWorkspaces() 
+
+runsDone = 0
+runsStarted = 0
+runsNeeded = 1000
+allResults = []
+def schedule(workspace_):
+    workspace_.command("stop")
+    '''for i, name in enumerate(problem['names']):
+        if name == 'random-seed':
+            #The NetLogo random seed requires a different syntax
+            workspace_.command('random-seed {}'.format(param_values[runsStarted][i]))
+        else:
+            #Otherwise, assume the input parameters are global variables
+            workspace_.command('set {0} {1}'.format(name, param_values[runsStarted][i]))'''
+    workspace_.setParamsRandom()
+    workspace_.command('setup')
+    workspace_.scheduleReportersAndRun(["ticks",'count sheep','count wolves'], 0,1,100,"go")    
+
+for i in range(0,1):
+    workspace = nl4py.newNetLogoHeadlessWorkspace()
+    workspace.openModel('Wolf Sheep Predation.nlogo')
+    schedule(workspace)
+    runsStarted = runsStarted + 1
+while (runsDone < runsNeeded):
+    for workspace in nl4py.getAllHeadlessWorkspaces(): 
+        wsr = workspace.getScheduledReporterResults()
+        print(wsr)
+        if wsr != []:
+            allResults.extend(wsr)
+            runsDone = runsDone + 1
+            print(runsDone)
+            if runsStarted < runsNeeded:
+                    schedule(workspace)
+                    runsStarted = runsStarted + 1
+stopTime = int(round(time.time() * 1000))
+print(stopTime - startTime)
