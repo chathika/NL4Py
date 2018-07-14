87746be8ddace7d752badd182fdd485941ce02be
chathika <chathikagunaratne@gmail.com>
2018-07-14 18:09:32 -0400

    Updated bat file to copy dependencies into python package

--- BuildServer.bat
+++ BuildServer.bat
@@ -2,5 +2,7 @@ javac -d bin -cp "C:/Users/ch328575/AppData/Local/Continuum/Anaconda2/share/py4j
 cp src/server/Manifest.txt bin
 cd bin
 jar cvfm NetLogoControllerServer.jar Manifest.txt bsearch nl4py
+xcopy /s "%~dp0\bin\NetLogoControllerServer.jar" "%~dp0\src\client\NL4Py\nl4py\nl4pyServer\"
+xcopy /s "%~dp0\lib\py4j0.10.6.jar" "%~dp0\src\client\NL4Py\nl4py\nl4pyServer\"
 cd ..
 
