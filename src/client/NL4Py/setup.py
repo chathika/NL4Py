4cb9a7c7d9d377ed8d704e7ae0e3c554f010aded
chathika <chathikagunaratne@gmail.com>
2020-03-01 17:03:27 +0000

    0.5.10 jar and setup.py

--- src/client/NL4Py/setup.py
+++ src/client/NL4Py/setup.py
@@ -2,7 +2,7 @@ from setuptools  import setup
 
 setup(
     name='NL4Py',
-    version='0.5.9',
+    version='0.5.10',
     author='Chathika Gunaratne',
     author_email='chathikagunaratne@gmail.com',
     packages=['nl4py', 'nl4py.test'],
