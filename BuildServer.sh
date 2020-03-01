7fa8a4eeedad6a9e471bc19eec2e82d834822855
chathika <chathikagunaratne@gmail.com>
2020-03-01 16:57:18 +0000

    Changes to build script to accommodate java files for NetLogo6.1/0 integration

--- BuildServer.sh
+++ BuildServer.sh
@@ -3,14 +3,14 @@ mkdir -p bin/NetLogo6.1/
 #Compile NetLogo 6.0 server
 #provide path to netlogo jar file (include jar file name)
 NetLogo6_0_Path="$1"
-javac -d bin/NetLogo6.0/ -cp "./lib/py4j0.10.6.jar:$NetLogo6_0_Path:." src/server_NetLogo6.0/server/bsearch/space/*.java src/server_NetLogo6.0/server/bsearch/nlogolink/*.java src/server_NetLogo6.0/server/*.java
+javac -d bin/NetLogo6.0/ -cp "./lib/py4j0.10.6.jar:$NetLogo6_0_Path:." src/server/bsearch/space/*.java src/server/bsearch/nlogolink/*.java src/server/*.java src/server/NetLogo6.0/*.java
 #provide path to netlogo jar file (include jar file name)
 #Compile NetLogo 6.1 server
 NetLogo6_1_Path="$2"
-javac -d bin/NetLogo6.1/ -cp "./lib/py4j0.10.6.jar:$NetLogo6_1_Path:." src/server_NetLogo6.1/server/bsearch/space/*.java src/server_NetLogo6.1/server/bsearch/nlogolink/*.java src/server_NetLogo6.1/server/*.java
+javac -d bin/NetLogo6.1/ -cp "./lib/py4j0.10.6.jar:$NetLogo6_1_Path:." src/server/bsearch/space/*.java src/server/bsearch/nlogolink/*.java src/server/*.java src/server/NetLogo6.1/*.java
 #Copy over manifests
-cp src/server_NetLogo6.0/server/Manifest.txt bin/NetLogo6.0/
-cp src/server_NetLogo6.1/server/Manifest.txt bin/NetLogo6.1/
+cp src/server/Manifest.txt bin/NetLogo6.0/
+cp src/server/Manifest.txt bin/NetLogo6.1/
 #Build jars 
 cd "bin/NetLogo6.0/"
 jar cvfm NetLogoControllerServer.jar Manifest.txt bsearch nl4py
