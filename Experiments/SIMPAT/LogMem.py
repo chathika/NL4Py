79d879c2b00bb05e5a93458bd40d979a885da9d1
chathika <chathikagunaratne@gmail.com>
2020-06-24 03:07:24 +0000

    Memory logger

--- Experiments/SIMPAT/LogMem.py
+++ Experiments/SIMPAT/LogMem.py
@@ -3,8 +3,8 @@ import os
 import time
 import csv
 
-def log_mem(connector,runsNeeded,ticksNeeded,rep):
-    mem_output_file = os.path.join("output","memory","{0}_runs{1}_ticks{2}_rep{3}.csv".format(connector,str(runsNeeded),str(ticksNeeded),str(rep)))
+def log_mem(connector,model,runsNeeded,ticksNeeded,rep):
+    mem_output_file = os.path.join("output","memory","{0}_model{1}_runs{2}_ticks{3}_rep{4}.csv".format(connector,str(model),str(runsNeeded),str(ticksNeeded),str(rep)))
     os.makedirs(os.path.dirname(mem_output_file), exist_ok=True)
     mem_stats = dict(psutil.virtual_memory()._asdict())
     with open (mem_output_file, "w+") as out:
