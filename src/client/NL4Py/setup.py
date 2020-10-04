fb0168ddff861f26b88682e42d80e904a7221a4b
chathika <chathikagunaratne@gmail.com>
2020-10-04 15:42:10 -0400

    0.7.0 Changes and setup

--- src/client/NL4Py/setup.py
+++ src/client/NL4Py/setup.py
@@ -2,7 +2,7 @@ from setuptools  import setup
 
 setup(
     name='NL4Py',
-    version='0.5.11',
+    version='0.7.0',
     author='Chathika Gunaratne',
     author_email='chathikagunaratne@gmail.com',
     packages=['nl4py', 'nl4py.test'],
